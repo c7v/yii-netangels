68aa08f95276333a510aee5590e67e8dea431fe2
Sokolovsky Artem <artem@sokolovsky.dev>
2021-12-29 11:46:07 +0400

    Реализованы методы получения список SSH-ключей пользователя

---
+++ src/requesters/ssh/Keys.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace c7v\yii_netangels\requesters\hosting;
+
+use c7v\yii_netangels\requesters\BaseRequester;
+use yii\base\InvalidConfigException;
+use yii\helpers\ArrayHelper;
+
+/**
+ * Class Site
+ * @package c7v\yii_netangels\requesters\hosting
+ */
+class Keys extends BaseRequester
+{
+    const URL = 'sshkeys/';
+
+    /**
+     * @param int $offset
+     * @param int $limit
+     * @return array
+     * @throws InvalidConfigException
+     * @throws \yii\httpclient\Exception
+     */
+    public function getList(int $offset, int $limit){
+        $request = self::$_httpClient
+            ->initHttpRequest(self::URL)
+            ->setMethod('GET')
+            ->setData([
+                'offset' => $offset,
+                'limit' => $limit,
+            ]);
+
+        return $this->processResponse($request);
+    }
+}
